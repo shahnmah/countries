a19cf343e6a0f4daae9bb7a529754bf37d203bee
shahnmah <shahn.mah@gmail.com>
2022-02-27 23:19:54 +0600

    country search ok

--- js/app.js
+++ js/app.js
@@ -5,6 +5,24 @@ const loadCountries = () =>{
     .then(response => response.json())
     .then(data => displayCountry(data))
 }
-const displayCountry = (country) =>{
+const displayCountry = (countries) =>{
+    const countryContainer = document.getElementById('country-container');
+    for(const country of countries){
+        const countryDetails = document.createElement('div')
+        countryDetails.innerHTML = ` 
+      
+            <div class="card my-4" style="width: 18rem;">
+                <img src="${country.flags.png}" class="card-img-top" alt="${country.name.common}">
+                <div class="card-body">
+                    <h3 class="card-title">${country.name.common}</h3>
+                <h4>Capital: ${country.capital}</h4>
+                <h5>Region: ${country.region}</h5>
+                <h5>Area: ${country.area}</h5>
+                <h5>Population: ${country.population}</h5>
+                </div>
+            </div> `
+    countryContainer.appendChild(countryDetails)
+    }
     
+
 }
